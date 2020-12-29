40a24886ffc47a044449505dd5f1fae405608dd7
csyuser <13540039093@163.com>
2020-12-29 17:16:06 +0800

    简化代码

--- index.js
+++ index.js
@@ -7,16 +7,72 @@ module.exports.add = async (title) => {
   //添加一个title任务
   list.push({title, done: false})
   //保存任务到文件
-  await db.save(list)
+  await db.write(list)
 }
 
 module.exports.clear = async () => {
-  await db.save([])
+  await db.write([])
 }
 
-module.exports.showAll = async () => {
-  //读取之前之前的tasks
-  let list = await db.read()
+function save (list,string){
+  db.write(list).then(() => {console.log(string + '成功')}).catch(() => {console.log(string + '失败')})
+}
+function markAsDone(list, index) {
+  list[index].done = true
+  save(list,'更改')
+}
+function markAsUndone(list, index) {
+  list[index].done = false
+  save(list,'更改')
+}
+function updateTitle(list, index) {
+  inquirer.prompt({
+    type: 'input',
+    name: 'newTitle',
+    message: '请输入新的任务名称',
+  }).then((answer) => {
+    list[index].title = answer['newTitle']
+    save(list,'更改')
+  })
+}
+function remove(list, index) {
+  list.splice(index, 1)
+  save(list,'更改')
+}
+function askForAction(list, index) {
+  const actions = {markAsDone,markAsUndone,updateTitle,remove}
+  inquirer
+    .prompt(
+      {
+        type: 'list',
+        name: 'action',
+        message: '请选择你想要操作的任务',
+        choices: [{name: '退出', value: 'quit'},
+          {name: '完成', value: 'markAsDone'},
+          {name: '未完成', value: 'markAsUndone'},
+          {name: '更改任务名称', value: 'updateTitle'},
+          {name: '删除', value: 'remove'},],
+      },
+    )
+    .then(answer2 => {
+      const action = actions[answer2.action]
+      action && action(list, index)
+    })
+}
+function askForCreateTask(list) {
+  inquirer.prompt({
+    type: 'input',
+    name: 'title',
+    message: '请输入任务名称',
+  }).then((answer) => {
+    list.push({
+      title: answer.title,
+      done: false
+    })
+    save(list,'添加')
+  })
+}
+function printTasks(list) {
   inquirer
     .prompt(
       {
@@ -34,56 +90,17 @@ module.exports.showAll = async () => {
       let index = answer.index
       if (index >= 0) {
         //操作现有的任务
-        inquirer
-          .prompt(
-            {
-              type: 'list',
-              name: 'action',
-              message: '请选择你想要操作的任务',
-              choices: [{name: '退出', value: 'quit'},
-                {name: '完成', value: 'markAsDone'},
-                {name: '未完成', value: 'markAsUndone'},
-                {name: '更改任务名称', value: 'updateTitle'},
-                {name: '删除', value: 'remove'},],
-            },
-          )
-          .then(answer2=>{
-            switch (answer2.action) {
-              case 'markAsDone':
-                list[index].done = true
-                break
-              case 'markAsUndone':
-                list[index].done = false
-                break
-              case 'updateTitle':
-                inquirer.prompt({
-                    type: 'input',
-                    name: 'newTitle',
-                    message: "请输入新的任务名称",
-                  }).then((answer) => {
-                  list[index].title = answer['newTitle']
-                  db.save(list)
-                });
-                break
-              case 'remove':
-                list.splice(index,1)
-                break
-            }
-            db.save(list)
-          })
+        askForAction(list, index)
       } else if (index === -2) {
         //新建一个任务
-        inquirer.prompt({
-          type: 'input',
-          name: 'title',
-          message: "请输入任务名称",
-        }).then((answer) => {
-          list.push({
-            title:answer.title,
-            done:false
-          })
-          db.save(list)
-        });
+        askForCreateTask(list)
       }
     })
+}
+
+module.exports.showAll = async () => {
+  //读取之前之前的tasks
+  let list = await db.read()
+  //打印之前的任务
+  printTasks(list)
 }
