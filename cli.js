e79bafee0ccd1076e3ad28132661647eb2e6e060
csyuser <13540039093@163.com>
2022-11-23 16:17:39 +0800

    xx

--- cli.js
+++ cli.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-const { program } = require('commander');
+const {program} = require('commander')
 const api = require('./index.js')
 const pkg = require('./package.json')
 
@@ -9,20 +9,21 @@ program
   .command('add')
   .description('add a task')
   .action((command) => {
+    console.log(command)
     let words = command.args.join(' ')
-    api.add(words).then(()=>{console.log('添加成功')}).catch(()=>{console.log('添加失败')})
-  });
+    api.add(words).then(() => {console.log('添加成功')}).catch(() => {console.log('添加失败')})
+  })
 program
   .command('clear')
   .description('clear tasks')
   .action(() => {
-    api.clear().then(()=>{console.log('删除成功')}).catch(()=>{console.log('删除失败')})
-  });
+    api.clear().then(() => {console.log('删除成功')}).catch(() => {console.log('删除失败')})
+  })
 program
   .command('list')
   .description('list tasks')
   .action(() => {
-   void api.showAll()
-  });
+    void api.showAll()
+  })
 
-program.parse(process.argv);
+program.parse(process.argv)
